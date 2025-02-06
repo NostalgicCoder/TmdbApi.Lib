d68c48716c96285a9621e76332d008a7b5c6c7ef
NostalgicCoder <spb.work@outlook.com>
2025-02-06 17:16:06 +0000

    Added better naming conventions to unit test and AAA comments

--- TmdbApi.Lib.Tests/TmdbTests.cs
+++ TmdbApi.Lib.Tests/TmdbTests.cs
@@ -10,41 +10,53 @@ namespace TmdbApi.Lib.Tests
         private ITmdb _tmdb = new Tmdb();
 
         [TestMethod]
-        public void CallCallTmdbApiShouldBeNull()
+        public void Tmdb_CallTmdbApi_ShouldBeNull()
         {
+            // Act
             Rootobject result = _tmdb.CallTmdbApi(string.Empty);
 
+            // Assert
             result.Should().BeNull();
         }
 
         [TestMethod]
-        public void CallCallTmdbApiLookForRoboCopResultShouldMatchExpected()
+        public void Tmdb_CallTmdbApi_ShouldMatchExpectedResult()
         {
+            // Arrange
             string query = "/search/movie?query=Robocop&include_adult=false&language=en-US&page=1";
 
+            // Act
             Rootobject result = _tmdb.CallTmdbApi(query);
 
+            // Assert
             result.results[0].original_title.Should().Be("RoboCop");
             result.results[0].release_date.Should().Be("1987-07-17");
         }
 
         [TestMethod]
-        public void CallCallTmdbApiLookForFakeMovieShouldMatchZero()
+        public void Tmdb_CallTmdbApi_ShouldHaveResultOfZeroAndNotBeNull()
         {
+            // Arrange
             string query = "/search/movie?query=FakeMovie&include_adult=false&language=en-US&page=1";
 
+            // Act
             Rootobject result = _tmdb.CallTmdbApi(query);
 
+            // Assert
             result.results.Count().Should().Be(0);
+            result.Should().NotBeNull();
         }
 
         [TestMethod]
-        public void CallSearchForFilmTvPersonResultShouldMatchExpected()
+        public void Tmdb_SearchForFilmTvPerson_ShouldMatchExpectedResult()
         {
+            // Arrange
             string keyword = "RoboCop";
 
+            // Act
             ResultReturn result = _tmdb.SearchForFilmTvPerson(keyword);
 
+            // Assert
             result.FilmResults.results[0].original_title.Should().Be("RoboCop");
             result.FilmResults.results[0].release_date.Should().Be("1987-07-17");
 
@@ -53,12 +65,15 @@ namespace TmdbApi.Lib.Tests
         }
 
         [TestMethod]
-        public void CallSearchForTvAndCreditsByIdResultShouldMatchExpected()
+        public void Tmdb_SearchForTvAndCreditsById_ShouldMatchExpectedResult()
         {
+            // Arrange
             int id = 93219;
 
+            // Act
             ResultReturn result = _tmdb.SearchForTvAndCreditsById(id);
 
+            // Assert
             result.TvIdResult.name.Should().Be("The Movies That Made Us"); 
             result.CreditsByTvId.id.Should().Be(93219);
         }
